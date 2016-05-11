2c6fbda07fefc81e740aca52d20be4ce5232743f
Kai Tao Cheng <ultimaboyz@hotmail.com>
2016-05-11 23:01:07 +0800

    Initial Commit

---
+++ tapper-extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-extreme
+//
+//  Created by Kai Tao Cheng on 11/05/2016.
+//  Copyright © 2016 Kai Tao Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
